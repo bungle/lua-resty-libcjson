ff415e35bb80898b99f403a3187ca97721130369
Aapo Talvensaari <aapo.talvensaari@gmail.com>
2015-10-30 15:03:18 +0200

    Fixed encoding benchmars.

--- lib/resty/libcjson/libcjsonenc.lua
+++ lib/resty/libcjson/libcjsonenc.lua
@@ -1,9 +1,11 @@
 local open, clock, format = io.open, os.clock, string.format
+local d = require "resty.libcjson".decode
 local e = require "resty.libcjson".encode
 local file = open("resty/libcjson/citylots.json", "rb")
 local content = file:read "*a"
 file:close()
+local t = d(content)
 local x = clock()
-e(content)
+e(t)
 local z = clock() - x
 print(format("Encoding Time: %.6f", z))
--- lib/resty/libcjson/luacjsonenc.lua
+++ lib/resty/libcjson/luacjsonenc.lua
@@ -1,9 +1,11 @@
 local open, clock, format = io.open, os.clock, string.format
+local d = require "cjson.safe".decode
 local e = require "cjson.safe".encode
 local file = open("resty/libcjson/citylots.json", "rb")
 local content = file:read "*a"
 file:close()
+local t = d(content)
 local x = clock()
-e(content)
+e(t)
 local z = clock() - x
 print(format("Encoding Time: %.6f", z))
